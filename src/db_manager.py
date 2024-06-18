2cbfd9477216f76f065ec57682fb60c8b497b508
skyl1 <skyl1.maxla2@mail.ru>
2024-06-19 00:07:57 +0500

    Добавлены функции для работы с классом

--- src/db_manager.py
+++ src/db_manager.py
@@ -27,3 +27,29 @@ class DBManager:
         self.cur.execute(query)
         return self.cur.fetchall()
 
+    def get_avg_salary(self):
+        """Получает среднюю зарплату по вакансиям."""
+        query = """
+        SELECT AVG(salary_from) FROM vacancies
+        """
+        self.cur.execute(query)
+        result = self.cur.fetchone()
+        return result[0] if result else None
+
+    def get_vacancies_with_higher_salary(self):
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        query = """
+        SELECT job_title, salary_from FROM vacancies
+        WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)
+        """
+        self.cur.execute(query)
+        return self.cur.fetchall()
+
+    def get_vacancies_with_keyword(self, keyword):
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python."""
+        query = """
+                        SELECT * FROM vacancies
+                        WHERE LOWER(job_title) LIKE %s
+                        """
+        self.cur.execute(query, ('%' + keyword.lower() + '%',))
+        return self.cur.fetchall()
